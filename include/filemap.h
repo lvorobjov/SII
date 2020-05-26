433bd49eb111d14964eb7281e867c014347b9ec4
lvorobjov <lev.vorobjov@rambler.ru>
2020-05-26 11:05:59 +0300

    filemap.h

---
+++ include/filemap.h
@@ -0,0 +1,19 @@
+/**
+ * @Author: Lev Vorobjev
+ * @Date:   12.04.2018
+ * @Email:  lev.vorobjev@rambler.ru
+ * @Filename: filemap.h
+ * @Last modified by:   Lev Vorobjev
+ * @Last modified time: 12.04.2018
+ * @License: MIT
+ * @Copyright: Copyright (c) 2018 Lev Vorobjev
+ */
+
+#ifndef FILEMAP_H
+#define FILEMAP_H
+
+#include <windows.h>
+
+LPTSTR ReadFileMultiByte(LPCTSTR lpszFilename);
+
+#endif

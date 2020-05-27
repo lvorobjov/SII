8db145fc8ecac6531c0d60b2df1f82d7b6cdb969
lvorobjov <lev.vorobjov@rambler.ru>
2020-05-27 13:14:40 +0300

    headers

---
+++ include/logic.h
@@ -0,0 +1,88 @@
+/**
+ * @Author: Lev Vorobjev
+ * @Date:   05.04.2018
+ * @Email:  lev.vorobjev@rambler.ru
+ * @Filename: logic.h
+ * @Last modified by:   Lev Vorobjev
+ * @Last modified time: 14.04.2018
+ * @License: MIT
+ * @Copyright: Copyright (c) 2018 Lev Vorobjev
+ */
+
+#ifndef LOGIC_H
+#define LOGIC_H
+
+#include "utils.h"
+#include "attribute.h"
+
+typedef struct _statement_t {
+	attribute_t* attr;
+	DWORD dwValue;
+} statement_t;
+
+typedef struct _rule_t {
+	int nPremises;
+	statement_t* premises;
+	statement_t conclusion;
+} rule_t;
+
+rule_t* rule_new(int nPremises);
+void rule_free(rule_t* r);
+void rule_list_free(list_t* &rules);
+
+typedef struct _goal_t {
+    attribute_t* attr;
+    rule_t* rule;
+} goal_t;
+
+goal_t* goal_new(attribute_t* attr, rule_t* rule);
+void goal_free(goal_t* goal);
+
+typedef struct _fact_t {
+    attribute_t* attr;
+    DWORD dwValue;
+    rule_t* rule;
+} fact_t;
+
+fact_t* fact_new(attribute_t* attr, DWORD dwValue, rule_t* rule);
+void fact_free(fact_t* fact);
+void fact_list_free(list_t* &facts);
+
+/**
+ * @deprecated
+ */
+typedef struct _logic_context_t {
+    int nAttrs;
+    attribute_t* attrs;
+    list_t* rules;
+} logic_context_t;
+
+logic_context_t* logic_ctx_new(int nAttrs);
+void logic_ctx_free(logic_context_t* ctx);
+
+typedef enum {
+    PROVE_OK, PROVE_FAIL, PROVE_QUERY
+} prove_result_t;
+
+/**
+ * Обратный логический вывод
+ * @param rules список правил
+ * @param goals стек целей, изначално включает глобальную цель
+ * @param facts список фактов
+ * @return ответ на глобальную цель или запрос ввода параметра (-1)
+ */
+prove_result_t prove(list_t* &rules, stack_t* &goals, list_t* &facts);
+
+/**
+ * Добаление новго факта в базу фактов
+ * @papam goals стек целей, в вершине стека добавляемый в БФ аттрибут
+ * @param facts база чактоы
+ * @param dwValue значение добавляемого аттрибута
+ */
+void add_answer(stack_t* &goals, list_t* &facts, DWORD dwValue);
+
+void rule_list_qsort(list_t* &rules);
+rule_t* rule_list_find(list_t* rules, attribute_t* attr);
+fact_t* fact_list_find(list_t* facts, attribute_t* attr);
+
+#endif
---
+++ include/utils.h
@@ -0,0 +1,76 @@
+/**
+ * @Author: Lev Vorobjev
+ * @Date:   05.04.2018
+ * @Email:  lev.vorobjev@rambler.ru
+ * @Filename: utils.h
+ * @Last modified by:   Lev Vorobjev
+ * @Last modified time: 11.04.2018
+ * @License: MIT
+ * @Copyright: Copyright (c) 2018 Lev Vorobjev
+ */
+
+#ifndef UTILS_H
+#define UTILS_H
+
+typedef struct _stack_t {
+    void* data;
+    struct _stack_t* next;
+} stack_t;
+
+void stack_push(stack_t* &stack, void* data);
+void* stack_pop(stack_t* &stack);
+
+typedef struct _queue_t {
+    stack_t* first;
+    stack_t** lastptr;
+} queue_t;
+
+void queue_enqueue(queue_t* &queue, void* data);
+void* queue_dequeue(queue_t* &queue);
+
+typedef struct _list_t {
+    void* data;
+    struct _list_t* next;
+} list_t;
+
+/**
+ * Добаление элемента в конец списка
+ * @param list список, в конец которого добавляется элемент
+ * @data данные добавляемого эоемента
+ * @return указатель на добавленный элемент
+ */
+list_t* list_add(list_t* &list, void* data);
+
+/**
+ * Соединение двух списков
+ * @param list список, в конец которого добавляется другой список
+ * @param othList добавляемый список
+ * @deprecated указатель на лемент списка, к которому
+ *  было присоединено начало другого списка при list != NULL
+ * @return указатель на добавленный список
+ */
+list_t* list_append(list_t* &list, list_t* othList);
+
+/**
+ * Удаление всех элементов списка
+ * @param list удаляемый список
+ */
+void list_free(list_t* &list);
+
+/**
+ * Инвертирование элементов списка
+ * @param list начало инвертируемого фрагмента
+ * @param end конец инвертируемого фрагмента (не инвертируемый)
+ * @return указатель на начало инвертированного фрагмента
+ */
+list_t* list_reverse(list_t* &list, list_t* end);
+
+/**
+ * Удаление элемента списка с заданным значением
+ * @param list список, в котором удаляется элемент
+ * @data содержание удаляемого элемента
+ * @return удаленный элемент
+ */
+list_t* list_remove(list_t* &list, void* data);
+
+#endif
